fdaf943f7733dab10c8e39c2725c5878648cea90
HebaAlhamaydh <Eng_heba_hamaydh@hotmail.com>
2022-04-19 16:42:55 +0300

    function code

--- app.js
+++ app.js
@@ -1,27 +1,77 @@
-//Enter name
-var Name =prompt("Enter your Name Please");
-//Enter gender
-var Gender =prompt("Whats your Gender male or female?");
-//Enter age
-var Age=prompt("What is your Age?");
-if(Age<=0 )
-{
-alert("Enter correct Age please!");
-}
-//skip welcome message confirm the message
-var conf=confirm("If you want to skip the welcoming message press OK please!!");
-//
+// //Enter name
+// var Name =prompt("Enter your Name Please");
+// //Enter gender
+// var Gender =prompt("Whats your Gender male or female?");
+// //Enter age
+// var Age=prompt("What is your Age?");
+// if(Age<=0 )
+// {
+// alert("Enter correct Age please!");
+// }
+// //skip welcome message confirm the message
+// var conf=confirm("If you want to skip the welcoming message press OK please!!");
+// //
 
-if(Gender=="female")
+// if(Gender=="female")
+// {
+//     alert("welcome "+ Name+" "+ "Ms");
+// }
+// else if(Gender=="male")
+// {
+//     alert("welcome "+ Name+" "+ "Mr");   
+// }
+// else{
+//     alert("Welcome "+Name+" "+"to Website!!")
+// }
+
+
+///////Validation function////
+function validationInput(inputUser)
 {
-    alert("welcome "+ Name+" "+ "Ms");
+   let resultInput;
+
+   if(inputUser!="Yes" && inputUser!="No"){
+    resultInput="invalid";
+     
+    }
+    else if (inputUser=="Yes") {
+         resultInput="Yes";
+   }
+   else if(inputUser=="No")
+   {   resultInput="No";}
+   return resultInput;
 }
-else if(Gender=="male")
+
+//////print fuction////
+function printArray(arr)
 {
-    alert("welcome "+ Name+" "+ "Mr");   
+    for(let i=0;i<arr.length;i++)
+    {
+    console.log(arr[i]);
+   }
 }
-else{
-    alert("Welcome "+Name+" "+"to Website!!")
-}
+//////Question//////
+let Nationality =prompt("Are you Jordanian a list (Yes/No)?");
+let Age =prompt("Are you between 20 and 35 years old a list (Yes/No)?");
+let Like =prompt("Do you like this website a list (Yes/No)?");
+
+let answerNationality=validationInput(Nationality);
+let answerAge=validationInput(Age);
+let answerLike=validationInput(Like);
+
+ let finalArray=[];
+ finalArray.push(answerNationality);
+ finalArray.push(answerAge);
+ finalArray.push(answerLike);
+ 
+
+ printArray(finalArray);
+
+ 
+ 
+
+
+
+
 
 
